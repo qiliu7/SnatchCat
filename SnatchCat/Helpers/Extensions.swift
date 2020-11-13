6b31f4b113952a67d11b5ba6f6e6d6c13b9c6fb0
Qi Liu <qil244@mail.usask.ca>
2020-11-13 00:31:14 -0600

    Add new extensions for encoding.

--- SnatchCat/Helpers/Extensions.swift
+++ SnatchCat/Helpers/Extensions.swift
@@ -38,3 +38,14 @@ extension String {
     var unicodes: [UInt32] { return unicodeScalars.map{$0.value} }
 }
 
+extension String {
+    var htmlDecoded: String {
+        let decoded = try? NSAttributedString(data: Data(utf8), options: [
+            .documentType: NSAttributedString.DocumentType.html,
+            .characterEncoding: String.Encoding.utf8.rawValue
+        ], documentAttributes: nil).string
+
+        return decoded ?? self
+    }
+}
+
