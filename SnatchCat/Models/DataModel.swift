5a47848c94bc286c5bf9f9691d8ba10fea6d1938
Qi Liu <qil244@mail.usask.ca>
2020-11-22 04:53:24 -0600

    Fix typo in file name.

---
+++ SnatchCat/Models/DataModel.swift
@@ -0,0 +1,33 @@
+//
+//  dataModel.swift
+//  SnatchCat
+//
+//  Created by Kappa on 2020-05-25.
+//  Copyright © 2020 qi. All rights reserved.
+//
+
+import UIKit
+
+struct Organization: Decodable {
+    let organization: OrganizationClass
+}
+
+struct OrganizationClass: Decodable {
+    let name: String
+    let email: String
+    let phone: String
+    let address: Address
+    let photos: [PhotoURL]
+}
+
+class Favorites {
+    static var catList = [CatResult]()
+}
+
+
+
+
+
+
+
+
